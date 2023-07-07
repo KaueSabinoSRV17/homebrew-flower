9a5c861a196b951b689d3e3e1c880b6d4bb0ccd4
KaueSabinoSRV17 <kauevillaca2018@gmail.com>
2023-07-06 23:14:46 -0200

    Brew formula update for Flower version 1.1.8

--- flower.rb
+++ flower.rb
@@ -5,11 +5,11 @@
 class Flower < Formula
   desc ""
   homepage "https://github.com/KaueSabinoSRV17/Flower"
-  version "1.0.8"
+  version "1.1.8"
 
   on_macos do
-    url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.0.8/Flower_1.0.8_darwin_all.tar.gz"
-    sha256 "5d5d54842ca02d02bae73a42ec9ab00794bbde3fdde9844a404782840360e0fa"
+    url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.1.8/Flower_1.1.8_darwin_all.tar.gz"
+    sha256 "6f2ab739259fdf31aaf8cb5bb14b5039337f04b223360ca45b96fff31c82d8e6"
 
     def install
       bin.install "Flower"
@@ -18,16 +18,16 @@ class Flower < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.0.8/Flower_1.0.8_linux_arm64.tar.gz"
-      sha256 "129e16c8a395043bb1d20ed9f6ffc2dc789c98ce7d9e6880529979e3220e07a8"
+      url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.1.8/Flower_1.1.8_linux_arm64.tar.gz"
+      sha256 "6bd9f1b2dd28624f2ad0ac1925879053d437d5ecddb63987b07d5163a99cca28"
 
       def install
         bin.install "flow"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.0.8/Flower_1.0.8_linux_amd64.tar.gz"
-      sha256 "be8e635cf02f959c286c8b8faf1c4287c539a46cc585989c630c9eb5f096da8d"
+      url "https://github.com/KaueSabinoSRV17/Flower/releases/download/1.1.8/Flower_1.1.8_linux_amd64.tar.gz"
+      sha256 "8885da485656c6cb616f51fe093e9dc23f14c91712e1fae598b8d32e801c85d8"
 
       def install
         bin.install "flow"
